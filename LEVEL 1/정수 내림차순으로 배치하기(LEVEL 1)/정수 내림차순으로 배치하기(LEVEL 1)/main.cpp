67fa090b060973c30d961875e2f95a1d2cf676dd
leed21542 <leed21542@naver.com>
2020-09-26 19:40:57 +0900

    2020.9.26 commit3

--- LEVEL 1/정수 내림차순으로 배치하기(LEVEL 1)/정수 내림차순으로 배치하기(LEVEL 1)/main.cpp
+++ LEVEL 1/정수 내림차순으로 배치하기(LEVEL 1)/정수 내림차순으로 배치하기(LEVEL 1)/main.cpp
@@ -7,9 +7,19 @@
 //
 
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    long long n = 118372;
+    long long answer = 0;
+    
+    
+    string s = to_string(n); //string으로 바꾼다.
+    sort(s.begin(), s.end(), greater<>()); //내림차순으로 정렬
+    answer = stoll(s);
+    
 }
