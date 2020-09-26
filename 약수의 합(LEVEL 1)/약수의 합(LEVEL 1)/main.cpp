214e12af70703253a2590a4f504db0a68c70f65a
leed21542 <leed21542@naver.com>
2020-09-26 18:01:13 +0900

    2020.9.26 추가commit

--- 약수의 합(LEVEL 1)/약수의 합(LEVEL 1)/main.cpp
+++ 약수의 합(LEVEL 1)/약수의 합(LEVEL 1)/main.cpp
@@ -12,6 +12,12 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
+    int n = 12;
+    int answer = 0;
     
+    for(int i=1; i<=n; i++)
+    {
+        if(n%i == 0)answer+=i;
+    }
     return 0;
 }
